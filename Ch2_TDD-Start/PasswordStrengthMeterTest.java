6b73e36e5b4c400d226eaeb8ef32daacad73b6dc
liarreez <112878394+liarreez@users.noreply.github.com>
2023-01-31 18:01:38 +0900

    Update PasswordStrengthMeterTest.java

--- Ch2_TDD-Start/PasswordStrengthMeterTest.java
+++ Ch2_TDD-Start/PasswordStrengthMeterTest.java
@@ -2,7 +2,13 @@ package chap02;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordStrengthMeterTest {
     @Test
-    void name() {}
+    void meetsAllCriteria_Then_Strong() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
+    }
 }
