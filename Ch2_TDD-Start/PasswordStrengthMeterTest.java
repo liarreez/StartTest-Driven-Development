c60ac225bc3320eb0ec6bd5b5983ab1c6e3c05b0
liarreez <112878394+liarreez@users.noreply.github.com>
2023-02-01 21:48:37 +0900

    Update PasswordStrengthMeterTest.java

--- Ch2_TDD-Start/PasswordStrengthMeterTest.java
+++ Ch2_TDD-Start/PasswordStrengthMeterTest.java
@@ -50,4 +50,9 @@ public class PasswordStrengthMeterTest {
     void meetsOnlyNumCriteria_Then_Weak() {
         assertStrength("12345", PasswordStrength.WEAK);
     }
+
+    @Test
+    void meetsOnlyUpperCriteria_Then_Weak() {
+        assertStrength("ABZEF", PasswordStrength.WEAK);
+    }
 }
