a85b456d741dcf48136420e337b00849cf54897f
liarreez <112878394+liarreez@users.noreply.github.com>
2023-02-01 22:02:44 +0900

    Update PasswordStrengthMeter.java

--- Ch2_TDD-Start/PasswordStrengthMeter.java
+++ Ch2_TDD-Start/PasswordStrengthMeter.java
@@ -3,13 +3,10 @@ package chap02;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
-        int metCounts = 0;
-        if (s.length() >=8) metCounts++;
-        if (meetsContainingNumberCriteria(s)) metCounts++;
-        if (meetsContainingUppercaseCriteria(s)) metCounts++;
-
+        int metCounts = getMetCriteriaCounts(s);
         if (metCounts <= 1) return PasswordStrength.WEAK;
         if (metCounts == 2) return PasswordStrength.NORMAL;
+
         return PasswordStrength.STRONG;
     }
 
@@ -29,5 +26,13 @@ public class PasswordStrengthMeter {
             }
         }
         return false;
+    }
+
+    private int getMetCriteriaCounts(String s) {
+        int metCounts = 0;
+        if (s.length() >=8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
+        return metCounts;
     }
 }
