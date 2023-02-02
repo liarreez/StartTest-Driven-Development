fd42564138e26af356020be54e039ebbc14d03d7
liarreez <112878394+liarreez@users.noreply.github.com>
2023-02-02 20:08:39 +0900

    Update ExpiryDateCalculatorTest.java

--- Ch3_TDD-Order/ExpiryDateCalculatorTest.java
+++ Ch3_TDD-Order/ExpiryDateCalculatorTest.java
@@ -6,12 +6,18 @@ import org.junit.jupiter.api.Test;
 public class ExpiryDateCalculatorTest {
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
-        LocalDate billingDate = LocalDate.of(2019,3,1);
-        int payAmount = 10_000;
+        assertExpiryDate(
+                LocalDate.of(2019,3,1), 10_000,
+                LocalDate.of(2019,4,1));
+        assertExpiryDate(
+                LocalDate.of(2019,5,5), 10_000,
+                LocalDate.of(2019,6,5));
+    }
 
+    private void assertExpiryDate(
+            LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        assertEquals(LocalDate.of(2019, 4, 1),expiryDate);
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, realExpiryDate);
     }
 }
